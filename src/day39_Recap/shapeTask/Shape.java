02c3b8b76b156efdbf031cc364b231e14e658d31
veys66 <veysdoganer@gmail.com>
2022-12-16 20:51:29 +0300

    day 39: Recap

--- src/day39_Recap/shapeTask/Shape.java
+++ src/day39_Recap/shapeTask/Shape.java
@@ -36,6 +36,10 @@ public class Shape {
 
     @Override
     public String toString() {
-        return super.toString();
+        return "Shape{" +
+                "name='" + name + '\'' +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
     }
 }
--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -1,5 +1,24 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
 
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+
+        if(side <=0){
+            System.err.println("Invalid Side: "+side);
+            System.exit(1);
+        }
+        this.side = side;
+    }
+
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
 }
