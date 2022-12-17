1b5b4fb2b694f853104a19d0f87091658a8f2516
veys66 <veysdoganer@gmail.com>
2022-12-17 17:08:19 +0300

    day 39: Recap

---
+++ src/day39_Recap/shapeTask/Circle.java
@@ -0,0 +1,24 @@
+package day39_Recap.shapeTask;
+
+public class Circle extends Shape{
+
+    private double radius;
+    public static double pi= 3.14;
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        if(radius <= 0){
+            System.err.println("Invalid Radius: "+radius);
+            System.exit(1);
+        }
+        this.radius=radius;
+    }
+
+    public Circle(String name, double radius) {
+        super(name);
+        setRadius(radius);
+    }
+}
