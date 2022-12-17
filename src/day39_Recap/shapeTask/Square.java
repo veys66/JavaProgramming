62d7a6b0b22820148cdfd8eb244657f26225b0f4
veys66 <veysdoganer@gmail.com>
2022-12-17 15:00:14 +0300

    day 39: Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -22,5 +22,22 @@ public class Square extends Shape{
         setSide(side);
     }
 
+    @Override
+    public double area() {
+        return side * side;
+    }
 
+    @Override
+    public double perimeter() {
+        return side * 4;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
