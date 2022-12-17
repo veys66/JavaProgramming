f1438d18db48cd5f29da40d6672636923a91dac1
veys66 <veysdoganer@gmail.com>
2022-12-17 17:11:55 +0300

    day 39: Recap

--- src/day39_Recap/shapeTask/Circle.java
+++ src/day39_Recap/shapeTask/Circle.java
@@ -21,4 +21,23 @@ public class Circle extends Shape{
         super("Circle");
         setRadius(radius);
     }
+
+    @Override
+    public double area() {
+        return radius * radius*pi;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*radius*pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                "area='" + area() + '\'' +
+                "perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
