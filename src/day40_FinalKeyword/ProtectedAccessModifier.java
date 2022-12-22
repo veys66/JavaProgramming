4effa177cf7af63f049c588fd222d90253f8287f
veys66 <veysdoganer@gmail.com>
2022-12-22 17:22:45 +0300

    day40_FinalKeyword

--- src/day40_FinalKeyword/ProtectedAccessModifier.java
+++ src/day40_FinalKeyword/ProtectedAccessModifier.java
@@ -1,13 +1,18 @@
 package day40_FinalKeyword;
 
-public class ProtectedAccessModifier {
+public class ProtectedAccessModifier{
 
     static String name1="Cydeo"; //default access modifier
     protected static String name2= "Wooden Spoon";//protected
 
 
+    static void method1(){
 
+    }
 
+    protected static void method2(){
+
+    }
 
 
 
