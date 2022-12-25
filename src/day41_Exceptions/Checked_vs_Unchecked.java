200b2462eb74d4ff78c07a2e28738e53af047856
veys66 <veysdoganer@gmail.com>
2022-12-25 12:40:37 +0300

    day41_Exceptions

---
+++ src/day41_Exceptions/Checked_vs_Unchecked.java
@@ -0,0 +1,23 @@
+package day41_Exceptions;
+
+public class Checked_vs_Unchecked {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        //unchecked exception
+        int a =10;
+        int b=0;
+
+        System.out.println(a/b);
+
+        System.out.println("Wooden Spoon");
+
+
+
+
+    }
+}
