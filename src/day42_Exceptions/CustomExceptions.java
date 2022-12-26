628d15c1ad71c6de0ceaac2e7b1e8ef555e3db6e
veys66 <veysdoganer@gmail.com>
2022-12-26 22:49:23 +0300

    day42_Exceptions

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,4 +1,33 @@
 package day42_Exceptions;
 
-public class CustomExceptions {
+ class FadyException extends RuntimeException{
+
+  public FadyException(){
+   super("Time for a short break");
+  }
+
+  public FadyException(String message){
+  super(message);
+  }
+}
+
+class NoBreakException extends Exception{
+
+}
+
+public class CustomExceptions{
+ public static void main(String[] args) throws NoBreakException {
+
+ //throw new FadyException("It's time for lunch break");
+
+
+   throw new NoBreakException();
+
+
+
+
+
+
+ }
+
 }
