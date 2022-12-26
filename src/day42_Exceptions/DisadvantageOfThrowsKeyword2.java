e0c00ed74225da0b262266e555b055b56a95d2c2
veys66 <veysdoganer@gmail.com>
2022-12-26 22:10:53 +0300

    day42_Exceptions

--- src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -1,14 +1,35 @@
 package day42_Exceptions;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
 public class DisadvantageOfThrowsKeyword2 {
-    public static void method1() throws FileNotFoundException {
-        new FileInputStream("");
+
+    public static void main(String[] args) throws Exception{
+        method1();
+        method2();
+        method3();
+
+        String str= null;
+        try{
+            System.out.println(str.charAt(0));
+        }catch (RuntimeException e){
+            e.printStackTrace();
+        }
     }
 
-    public static void method2() throws FileNotFoundException {
+    public static void method(){
+        try {
+            method1();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void method1() throws InterruptedException {
+       // new FileInputStream("");
+        Thread.sleep(3000);
+    }
+
+    public static void method2() throws FileNotFoundException, InterruptedException {
         method1();
     }
 
