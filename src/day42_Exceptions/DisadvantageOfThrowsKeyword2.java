8d0f54c81f383641d51a8db78d4ee44a50547527
veys66 <veysdoganer@gmail.com>
2022-12-26 21:55:09 +0300

    day42_Exceptions

---
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword2.java
@@ -0,0 +1,25 @@
+package day42_Exceptions;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class DisadvantageOfThrowsKeyword2 {
+    public static void method1() throws FileNotFoundException {
+        new FileInputStream("");
+    }
+
+    public static void method2() throws FileNotFoundException {
+        method1();
+    }
+
+    public static void method3() throws FileNotFoundException, InterruptedException {
+        method2();
+        Thread.sleep(1000);
+    }
+
+    public static void method4() throws Exception {
+        method3();
+    }
+
+
+}
