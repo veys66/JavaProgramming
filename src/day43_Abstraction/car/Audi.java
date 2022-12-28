74add5db81264b1cb9b0a51b23b4c99d91b4f77e
veys66 <veysdoganer@gmail.com>
2022-12-28 23:35:03 +0300

    day43_Abstraction;

--- src/day43_Abstraction/car/Audi.java
+++ src/day43_Abstraction/car/Audi.java
@@ -1,6 +1,6 @@
 package day43_Abstraction.car;
 
-public class Audi extends Car {
+public final class Audi extends Car {
 
     public Audi(String brand, String model, String color, int year, double price) {
         super("Audi", model, color, year, price);
---
+++ src/day43_Abstraction/car/Tesla.java
@@ -0,0 +1,13 @@
+package day43_Abstraction.car;
+
+public final class Tesla extends Car {
+
+
+    public Tesla(String brand, String model, String color, int year, double price) {
+        super("Tesla", model, color, year, price);
+    }
+
+    public void start() {
+
+    }
+}
