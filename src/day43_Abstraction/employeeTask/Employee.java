81145b3c0c706bc1cd3724da744f147172c05fbd
veys66 <veysdoganer@gmail.com>
2022-12-30 19:46:34 +0300

    day43_Abstraction;

--- src/day43_Abstraction/employeeTask/Employee.java
+++ src/day43_Abstraction/employeeTask/Employee.java
@@ -7,9 +7,12 @@ public abstract class Employee extends Person{
 
     public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
         super(name, age, gender);
+        if(id <=0){
+            throw new RuntimeException("Invalid ID: "+id);
+        }
         this.id = id;
-        this.jobTitle = jobTitle;
-        this.salary = salary;
+        setJobTitle(jobTitle);
+        setSalary(salary);
     }
 
     public int getId() {
@@ -34,8 +37,16 @@ public abstract class Employee extends Person{
 
     public abstract void work();
 
-
-
-
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name=" + getName() +
+                "age=" + getAge() +
+                "gender=" + getGender() +
+                "id=" + id +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
 
