c5c2ba0c3634e274f78ce559c5e12e67a41786cc
veys66 <veysdoganer@gmail.com>
2022-12-30 17:13:03 +0300

    day43_Abstraction;

---
+++ src/day43_Abstraction/employeeTask/Employee.java
@@ -0,0 +1,41 @@
+package day43_Abstraction.employeeTask;
+
+public abstract class Employee extends Person{
+    private final int id;
+    private String jobTitle;
+    private double salary;
+
+    public Employee(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender);
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public abstract void work();
+
+
+
+
+}
+
