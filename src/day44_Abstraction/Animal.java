4add58b78d5bf8b5f6da73329a6d463a487e8478
veys66 <veysdoganer@gmail.com>
2023-01-03 21:20:42 +0300

    day44_Abstraction

--- src/day44_Abstraction/Animal.java
+++ src/day44_Abstraction/Animal.java
@@ -2,10 +2,69 @@ package day44_Abstraction;
 
 public class Animal {
 
+    private String name;
+    private final String breed;
+    private final char gender;
+    private int age;
+    private String size;
+    private final String color;
 
+    public final static boolean canBreathe;
 
+    static {
+        canBreathe=true;
+    }
 
+    public Animal(String name, String breed, char gender, int age, String size, String color) {
+        setName(name);
+        this.breed = breed;
+        if(!(gender=='M' || gender=='F')){
+            throw new RuntimeException("Invalid gender: "+gender);
+        }
+        this.gender = gender;
+        setAge(age);
+        setSize(size);
+        this.color = color;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.isEmpty()){
+            throw new RuntimeException("Invalid Name");
+        }
+        this.name=name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
 
 
