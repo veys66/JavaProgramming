d4f4b6a780c2ea7724b2ddbdf2ef28ea5a67697a
veys66 <veysdoganer@gmail.com>
2023-01-03 20:59:57 +0300

    day44_Abstraction

---
+++ src/day44_Abstraction/Animal.java
@@ -0,0 +1,28 @@
+package day44_Abstraction;
+
+public class Animal {
+
+
+
+
+
+
+
+
+}
+/*
+Animal Task:
+Create an abstract class named Animal:
+Variables:
+name, breed(final), gender(final), age, size, color(final)
+
+Encapsulate all the fields
+
+Add a cosntructor that can set all the fields
+
+Methods:
+eat(); ==> different animals eat
+different foods
+drink() ==> all the animals drink water
+toString() ==> to display the full info of the animal
+ */
