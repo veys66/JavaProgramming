998b5101c10685bb8b1c5673be03de4af84fb097
veys66 <veysdoganer@gmail.com>
2023-01-03 22:20:03 +0300

    day44_Abstraction

--- src/day44_Abstraction/animalTask/Parrot.java
+++ src/day44_Abstraction/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public class Parrot extends Animal{
+public final class Parrot extends Animal{
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
