5a5a295422accd0cfa44d4986c90200d265e0db3
veys66 <veysdoganer@gmail.com>
2023-01-04 20:01:04 +0300

    day45_Abstraction

--- src/day44_Abstraction/animalTask/Playable.java
+++ src/day44_Abstraction/animalTask/Playable.java
@@ -2,6 +2,10 @@ package day44_Abstraction.animalTask;
 
 public interface Playable {
 
+    static boolean isFriendly=true;//static & final by default
+
+
+
     void play();//abstract by default
 
 
