12f76a9d983395be3ad23ae23d726fbaa7659cde
veys66 <veysdoganer@gmail.com>
2023-01-04 20:10:10 +0300

    day45_Abstraction

--- src/day44_Abstraction/animalTask/Cat.java
+++ src/day44_Abstraction/animalTask/Cat.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Cat extends Animal{
+public final class Cat extends Animal implements Playable{
 
     public Cat(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -13,5 +13,11 @@ public final class Cat extends Animal{
 
     public void Meow(){
         System.out.println(getName()+" is Meowing");
+    }
+
+
+    @Override
+    public void play() {
+
     }
 }
--- src/day44_Abstraction/animalTask/Parrot.java
+++ src/day44_Abstraction/animalTask/Parrot.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public final class Parrot extends Animal{
+public final class Parrot extends Animal implements Playable{
     public Parrot(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
     }
@@ -8,5 +8,10 @@ public final class Parrot extends Animal{
     @Override
     public void eat() {
         System.out.println(getName()+" is eating chocolate");
+    }
+
+    @Override
+    public void play() {
+
     }
 }
