f9f6d3dbddc5d2e5d79e4427f7ff8f3662df9faa
veys66 <veysdoganer@gmail.com>
2023-01-05 22:23:16 +0300

    day45_Abstraction

--- src/day45_Abstraction/shape/Shape.java
+++ src/day45_Abstraction/shape/Shape.java
@@ -1,4 +1,27 @@
 package day45_Abstraction.shape;
 
-public class Shape {
+public abstract class Shape {
+
+    private final String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public abstract double area();
+
+    public abstract double perimeter();
+
+
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "name='" + name + '\'' +
+                '}';
+    }
 }
+
