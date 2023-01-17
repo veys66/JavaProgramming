3957a7427e75e509ee50755193789a7b71039dae
veys66 <veysdoganer@gmail.com>
2023-01-17 22:21:32 +0300

    day48_Collections

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -1,7 +1,18 @@
 package day49_Collections;
 
+import java.util.*;
+
 public class SetInterfacePractice {
     public static void main(String[] args) {
+
+        Set<Integer> hashSet= new HashSet<>(); //fastest performance
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+
+        Set<Integer> linkedHashSet= new LinkedHashSet<>();
+
+
+        Set<Integer> treeSet= new TreeSet<>();
 
 
 
