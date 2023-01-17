dd95fc40d311ef7eff4be527bc3f125a2cb704ff
veys66 <veysdoganer@gmail.com>
2023-01-17 12:04:21 +0300

    day48_Collections

---
+++ src/day48_Collections/ListIntro.java
@@ -0,0 +1,12 @@
+package day48_Collections;
+
+import java.util.List;
+import java.util.Vector;
+
+public class ListIntro {
+
+    public static void main(String[] args) {
+
+        List<Integer> list1= new Vector<>();
+    }
+}
