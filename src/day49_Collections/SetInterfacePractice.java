89b213d3a35c4b4fd990da5f3b5362893163c654
veys66 <veysdoganer@gmail.com>
2023-01-17 22:29:24 +0300

    day48_Collections

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -5,8 +5,29 @@ import java.util.*;
 public class SetInterfacePractice {
     public static void main(String[] args) {
 
+
+        List<Integer> list= new ArrayList<>();
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        list.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("list = " + list);
+        System.out.println(list.get(4));
+
+
         Set<Integer> hashSet= new HashSet<>(); //fastest performance
         hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        hashSet.addAll(Arrays.asList(null, null, null, null));
+
+
+
+        //System.out.println(hashSet);
+        System.out.println("hashSet = " + hashSet);
 
 
         Set<Integer> linkedHashSet= new LinkedHashSet<>();
