efcfb7508d4bd013af4e7e4e3be24f92e6494c13
veys66 <veysdoganer@gmail.com>
2023-01-17 22:40:14 +0300

    day49_Collections

--- src/day49_Collections/SetInterfacePractice.java
+++ src/day49_Collections/SetInterfacePractice.java
@@ -31,14 +31,28 @@ public class SetInterfacePractice {
 
 
         Set<Integer> linkedHashSet= new LinkedHashSet<>();
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        linkedHashSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+
+        System.out.println("linkedHashSet = " + linkedHashSet);
 
 
         Set<Integer> treeSet= new TreeSet<>();
-
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
+        treeSet.addAll(Arrays.asList(10, 200, 300, 40, 90));
 
 
 
 
 
     }
+
+
 }
