6f44f677a29ecbebab663e3b37eac8ccd182f963
veys66 <veysdoganer@gmail.com>
2023-01-25 21:50:59 +0300

    day50_Collections_Map

--- src/day50_Collections_Map/IterablePractice2.java
+++ src/day50_Collections_Map/IterablePractice2.java
@@ -13,7 +13,31 @@ public class IterablePractice2 {
         names.addAll(Arrays.asList("Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"));
 
         //remove all the names "ahmed"
+
        Iterator<String > it = names.iterator();
+
+       while(it.hasNext()) {
+           if (it.next().equalsIgnoreCase("ahmed")) ;
+           {
+               it.remove();
+           }
+       }
+
+        System.out.println(names);
+
+        System.out.println("----------------------------------------------------------------------------------------------");
+        List<String> names2= new ArrayList<>();
+        names2.addAll(Arrays.asList("Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"));
+
+
+        for(Iterator<String> i=names.iterator(); it.hasNext();){
+            if(i.next().equalsIgnoreCase("ahmed")){
+                i.remove();
+            }
+        }
+
+        System.out.println(names2);
+
 
     }
 }
