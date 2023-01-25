d7775300bf367f6824c387f8c3a8cdf14b28fa34
veys66 <veysdoganer@gmail.com>
2023-01-25 21:37:47 +0300

    day50_Collections_Map

--- src/day50_Collections_Map/IterablePractice.java
+++ src/day50_Collections_Map/IterablePractice.java
@@ -59,5 +59,13 @@ public class IterablePractice {
         }
 
         System.out.println(list3);
+
+        System.out.println("==================================================================================");
+
+        List<Integer> list4 = new ArrayList<>(Arrays.asList(1,2,3,4,5,1,2,3,4,5,1,2,3,4,5));
+
+        list4.removeIf(each ->each <4);
+
+        System.out.println(list4);
     }
 }
