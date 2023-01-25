6cfaafb848ac643a3c3824cf5a40ad039ab31e31
veys66 <veysdoganer@gmail.com>
2023-01-25 21:42:47 +0300

    day50_Collections_Map

---
+++ src/day50_Collections_Map/IterablePractice2.java
@@ -0,0 +1,17 @@
+package day50_Collections_Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IterablePractice2 {
+
+    public static void main(String[] args) {
+
+        List<String> names= new ArrayList<>();
+        names.addAll(Arrays.asList("Ahmed", "ahmed", "aHmEd", "John", "Ercon", "Daniel", "Mustafa", "Mohammed", "Ahmed", "ahmed", "Yuliia", "Chris"));
+
+        //remove all the names "ahmed"
+
+    }
+}
