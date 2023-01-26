bfc88483fc2da898ca53ac35dac3fea5bf1a5468
veys66 <veysdoganer@gmail.com>
2023-01-26 14:25:48 +0300

    day51_Map

--- src/day51_Map/MapClasses.java
+++ src/day51_Map/MapClasses.java
@@ -17,13 +17,31 @@ public class MapClasses {
         hashMap.put(6, "Ahmet");
         hashMap.put(7, "Ahmet");
         hashMap.put(8, "Ahmet");
+        hashMap.put(null, "Cihad");
 
 
         System.out.println("hashMap = " + hashMap);
 
         Map<Integer, String> linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put(10, "Arthur");
+        linkedHashMap.put(20, "George");
+        linkedHashMap.put(3, "Jack");
+        linkedHashMap.put(40, "Emma");
+        linkedHashMap.put(5, "Isabella");
+        linkedHashMap.put(5, "Ahmet");
+        linkedHashMap.put(null, "Hulya");
+
+        System.out.println("linkedHashMap = " + linkedHashMap);
 
         Map<Integer, String> treeMap= new TreeMap<>();
+        treeMap.put(10, "Arthur");
+        treeMap.put(20, "George");
+        treeMap.put(3, "Jack");
+        treeMap.put(40, "Emma");
+        treeMap.put(5, "Isabella");
+        treeMap.put(5, "Ahmet");
+        treeMap.put(null, "Hulya");
+
 
         Map<Integer, String> hashtable = new Hashtable<>();
 
