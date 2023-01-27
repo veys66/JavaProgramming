0dfddaa645a43fdf3ec74530ceda1c471c22f570
veys66 <veysdoganer@gmail.com>
2023-01-27 16:33:28 +0300

    day51_Map

--- src/day51_Map/TestEnum.java
+++ src/day51_Map/TestEnum.java
@@ -5,7 +5,29 @@ public class TestEnum {
 
         //String browser = "Wooden Spoon";
 
-        Browser bowser = Browser.SAFARI;
+        Browser browser = Browser.SAFARI;
+
+        switch (browser){
+            case FIREFOX:
+                System.out.println("Firefox is selected");
+                break;
+
+            case SAFARI:
+                System.out.println("Safari is selected");
+                break;
+
+            case OPERA:
+                System.out.println("Opera is selected");
+                break;
+
+            case EDGE:
+                System.out.println("Edge is selected");
+                break;
+
+            default:
+                System.out.println("Chrome is selected");
+
+        }
 
 
 
