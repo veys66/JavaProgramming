8bafc14ef4de5be50a1e6f283735d596608ab706
veys66 <veysdoganer@gmail.com>
2023-01-27 16:31:07 +0300

    day51_Map

---
+++ src/day51_Map/TestEnum.java
@@ -0,0 +1,14 @@
+package day51_Map;
+
+public class TestEnum {
+    public static void main(String[] args) {
+
+        //String browser = "Wooden Spoon";
+
+        Browser bowser = Browser.SAFARI;
+
+
+
+
+    }
+}
